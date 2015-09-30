ca412d8e4cd25468eb814b3050cdab3e4339658d
bsp <brendan.sapience@gmail.com>
2015-09-30 15:20:52 -0400

    improving error management

--- src/com/automic/specifics/GoRunCommand.java
+++ src/com/automic/specifics/GoRunCommand.java
@@ -60,54 +60,50 @@ public class GoRunCommand {
 			else{
 				
 				UC4Object obj = Objbroker.common.openObject(ObjectName, false);
-				Job job = (Job) obj; 
-				
-				System.out.println(" ++ Updating Job Definition for Job: " + ObjectName);
+				if(obj != null){
+					Job job = (Job) obj; 
+					
+					System.out.println(" ++ Updating Job Definition for Job: " + ObjectName);
 
-				
-				if(ProcessSpecificCLI.ADDVARIABLE && !ProcessSpecificCLI.ERRORINVARIABLE){
 					
-					System.out.println("  => Adding Variable: "+ProcessSpecificCLI.VARNAME +" with Value: "+ProcessSpecificCLI.VARVALUE);
-					job.values().addValue(ProcessSpecificCLI.VARNAME, ProcessSpecificCLI.VARVALUE,false);
-				}
-				
-				if(ProcessSpecificCLI.REMOVEVARIABLE && !ProcessSpecificCLI.ERRORINVARIABLE){
-					System.out.println("  => Removing Variable: "+ProcessSpecificCLI.VARNAME);
-					job.values().removeValue(ProcessSpecificCLI.VARNAME);
-				}
-				
-				if(ProcessSpecificCLI.UPDATEVARIABLE && !ProcessSpecificCLI.ERRORINVARIABLE){
-					System.out.println("  => Updating Variable: "+ProcessSpecificCLI.VARNAME +" with Value: "+ProcessSpecificCLI.VARVALUE);
-					job.values().removeValue(ProcessSpecificCLI.VARNAME);
+					if(ProcessSpecificCLI.ADDVARIABLE && !ProcessSpecificCLI.ERRORINVARIABLE){
+						
+						System.out.println("  => Adding Variable: "+ProcessSpecificCLI.VARNAME +" with Value: "+ProcessSpecificCLI.VARVALUE);
+						job.values().addValue(ProcessSpecificCLI.VARNAME, ProcessSpecificCLI.VARVALUE,false);
+					}
 					
-					job.values().addValue(ProcessSpecificCLI.VARNAME, ProcessSpecificCLI.VARVALUE,true);
+					if(ProcessSpecificCLI.REMOVEVARIABLE && !ProcessSpecificCLI.ERRORINVARIABLE){
+						System.out.println("  => Removing Variable: "+ProcessSpecificCLI.VARNAME);
+						job.values().removeValue(ProcessSpecificCLI.VARNAME);
+					}
+					
+					if(ProcessSpecificCLI.UPDATEVARIABLE && !ProcessSpecificCLI.ERRORINVARIABLE){
+						System.out.println("  => Updating Variable: "+ProcessSpecificCLI.VARNAME +" with Value: "+ProcessSpecificCLI.VARVALUE);
+						job.values().removeValue(ProcessSpecificCLI.VARNAME);
+						
+						job.values().addValue(ProcessSpecificCLI.VARNAME, ProcessSpecificCLI.VARVALUE,true);
+					}
+					
+					if(!ProcessSpecificCLI.JOBLOGIN.equals("")){System.out.println("  => Changing Login to: "+ProcessSpecificCLI.JOBLOGIN);job.attributes().setLogin(new UC4ObjectName(ProcessSpecificCLI.JOBLOGIN));}
+					if(!ProcessSpecificCLI.JOBHOST.equals("")){System.out.println("  => Changing Host to: "+ProcessSpecificCLI.JOBHOST);job.attributes().setHost(new UC4HostName(ProcessSpecificCLI.JOBHOST));}
+					if(!ProcessSpecificCLI.JOBPROCESS.equals("")){System.out.println("  => Changing Process");job.setProcess(ProcessSpecificCLI.JOBPROCESS);}
+					if(!ProcessSpecificCLI.JOBPREPROCESS.equals("")){System.out.println("  => Changing Pre-Process");job.setPreProcess(ProcessSpecificCLI.JOBPREPROCESS);}
+					if(!ProcessSpecificCLI.JOBPOSTPROCESS.equals("")){System.out.println("  => Changing Post-Process");job.setPostProcess(ProcessSpecificCLI.JOBPOSTPROCESS);}
+					if(!ProcessSpecificCLI.JOBTITLE.equals("")){System.out.println("  => Changing Title to: "+ProcessSpecificCLI.JOBTITLE);job.header().setTitle(ProcessSpecificCLI.JOBTITLE);}
+					if(!ProcessSpecificCLI.JOBQUEUE.equals("")){System.out.println("  => Changing Queue to: "+ProcessSpecificCLI.JOBQUEUE);job.attributes().setQueue(new UC4ObjectName(ProcessSpecificCLI.JOBQUEUE));}
+					if(!ProcessSpecificCLI.JOBTZ.equals("")){System.out.println("  => Changing Timezone to: "+ProcessSpecificCLI.JOBTZ);job.attributes().setTimezone(new UC4TimezoneName(ProcessSpecificCLI.JOBTZ));}
+					if(ProcessSpecificCLI.JOBPRIORITY != 0){System.out.println("  => Changing Priority to: "+ProcessSpecificCLI.JOBPRIORITY);job.attributes().setPriority(ProcessSpecificCLI.JOBPRIORITY);}
+					if(ProcessSpecificCLI.JOBGENERATEATRUNTIME){System.out.println("  => Changing Generate At Runtime");job.attributes().setGenerateAtRuntime(true);}
+					if(ProcessSpecificCLI.JOBACTIVE){System.out.println("  => Activate Job");job.header().setActive(true);}
+					if(ProcessSpecificCLI.JOBINACTIVE){System.out.println("  => Deactivate Job");job.header().setActive(false);}
+					
+					System.out.println("");
+					Objbroker.common.saveAndCloseObject(job);
+				}else{
+					System.out.println("\n -- Error: Could Not Open Object " + ObjectName);
+					System.out.println(" -- Hint: Object may already be open by another user / session.");
 				}
-				
-				if(!ProcessSpecificCLI.JOBLOGIN.equals("")){System.out.println("  => Changing Login to: "+ProcessSpecificCLI.JOBLOGIN);job.attributes().setLogin(new UC4ObjectName(ProcessSpecificCLI.JOBLOGIN));}
-				if(!ProcessSpecificCLI.JOBHOST.equals("")){System.out.println("  => Changing Host to: "+ProcessSpecificCLI.JOBHOST);job.attributes().setHost(new UC4HostName(ProcessSpecificCLI.JOBHOST));}
-				if(!ProcessSpecificCLI.JOBPROCESS.equals("")){System.out.println("  => Changing Process");job.setProcess(ProcessSpecificCLI.JOBPROCESS);}
-				if(!ProcessSpecificCLI.JOBPREPROCESS.equals("")){System.out.println("  => Changing Pre-Process");job.setPreProcess(ProcessSpecificCLI.JOBPREPROCESS);}
-				if(!ProcessSpecificCLI.JOBPOSTPROCESS.equals("")){System.out.println("  => Changing Post-Process");job.setPostProcess(ProcessSpecificCLI.JOBPOSTPROCESS);}
-				if(!ProcessSpecificCLI.JOBTITLE.equals("")){System.out.println("  => Changing Title to: "+ProcessSpecificCLI.JOBTITLE);job.header().setTitle(ProcessSpecificCLI.JOBTITLE);}
-				if(!ProcessSpecificCLI.JOBQUEUE.equals("")){System.out.println("  => Changing Queue to: "+ProcessSpecificCLI.JOBQUEUE);job.attributes().setQueue(new UC4ObjectName(ProcessSpecificCLI.JOBQUEUE));}
-				if(!ProcessSpecificCLI.JOBTZ.equals("")){System.out.println("  => Changing Timezone to: "+ProcessSpecificCLI.JOBTZ);job.attributes().setTimezone(new UC4TimezoneName(ProcessSpecificCLI.JOBTZ));}
-				if(ProcessSpecificCLI.JOBPRIORITY != 0){System.out.println("  => Changing Priority to: "+ProcessSpecificCLI.JOBPRIORITY);job.attributes().setPriority(ProcessSpecificCLI.JOBPRIORITY);}
-				if(ProcessSpecificCLI.JOBGENERATEATRUNTIME){System.out.println("  => Changing Generate At Runtime");job.attributes().setGenerateAtRuntime(true);}
-				if(ProcessSpecificCLI.JOBACTIVE){System.out.println("  => Activate Job");job.header().setActive(true);}
-				if(ProcessSpecificCLI.JOBINACTIVE){System.out.println("  => Deactivate Job");job.header().setActive(false);}
-				
-				System.out.println("");
-				Objbroker.common.saveAndCloseObject(job);
 			}
-			
-
-			
-			
-		}
-			
-		
-		
-		
-	
+				}
 	}
 }
